d140e5ec4ba0586762885570a198a16ab0be18a0
ya-shine <57051953+ya-shine@users.noreply.github.com>
2020-06-04 15:51:17 +0900

    edit buyers controller

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,35 @@
 class BuyersController < ApplicationController
+
+    def index
+        @buyers = Buyer.all
+    end
+
+    def new
+        @buyer = Buyer.new
+    end
+
+    def create
+        @buyer = Buyer.new(buyer_params)
+        @buyer.save
+    end
+
+    def edit
+        @buyer = Buyer.find(params[:id])
+    end
+
+    def update
+        buyer = Buyer.find(params[:id])
+        buyer.update(buyer_params)
+    end
+
+    def destroy
+        buyer = Buyer.find(params[:id]
+        buyer.destroy)
+    end
+
+    private
+    def buyer_params
+        params.require(:buyer).permit(:name)
+    end
+    
 end
