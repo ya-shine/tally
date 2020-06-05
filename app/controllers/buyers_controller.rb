8c37b005347c36193b91fa18d9d13473be8c54bc
ya-shine <57051953+ya-shine@users.noreply.github.com>
2020-06-05 14:18:59 +0900

    fixed

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -29,7 +29,7 @@ class BuyersController < ApplicationController
 
     private
     def buyer_params
-        params.require(:buyer).permit(:name)
+        params.require(:buyer).permit!
     end
 
 end
