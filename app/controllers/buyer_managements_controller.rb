66cc33c27c80fc3dedbdeb7ef4743ada75caa235
ya-shine <57051953+ya-shine@users.noreply.github.com>
2020-06-07 18:09:35 +0900

    edit buyer_management controller

--- app/controllers/buyer_managements_controller.rb
+++ app/controllers/buyer_managements_controller.rb
@@ -1,8 +1,34 @@
 class BuyerManagementsController < ApplicationController
 
     def index
+        @q = BuyerManagement.ransack(params[:q])
         @buyers = Buyer.all
         @products = Product.all
+        @buyer_managements = @q.result
     end
 
+    def search
+        @q = BuyerManagement.search(search_params)
+        @buyer_managemments = @q.result
+    end
+
+    def new
+        @buyer_management = BuyerManagement.new
+    end
+
+    def create
+        @buyer_management = BuyerManagement.new(buyer_management_params)
+        @buyer_management.save
+        redirect_to root_path
+    end
+
+    private
+    def buyer_management_params
+        params.permit(:buyer_id, :product_id)
+    end
+
+    def search_params
+        params.require(:q).rermit!
+    end
+    
 end
