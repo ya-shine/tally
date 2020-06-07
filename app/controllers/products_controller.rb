574c1774886248acd6addff1fbf6641d7e175052
ya-shine <57051953+ya-shine@users.noreply.github.com>
2020-06-07 14:29:58 +0900

    difine edit action in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,10 @@ class ProductsController < ApplicationController
         @product.save
     end
 
+    def edit
+        @product = Product.find(params[:id])
+    end
+
     private
     def product_params
         params.require(:product).permit(:name, :price)
