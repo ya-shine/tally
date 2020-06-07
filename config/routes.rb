c9676102bbbcfc00148ac5811e7c0a40b3897f75
ya-shine <57051953+ya-shine@users.noreply.github.com>
2020-06-07 15:12:13 +0900

    routing setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "home#index"
   resources :buyers, except: :show
   resources :products, except: :show
+  resources :buyer_managements, only: [:index, :new, :create]
 end
